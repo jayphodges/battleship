24f60312e65f813e0c181827f70a632eaa14ef9f
jayphodges <jayphodges@gmail.com>
2017-07-15 21:11:40 -0600

    Add game menu and updated instructions

--- lib/battleship.rb
+++ lib/battleship.rb
@@ -12,47 +12,49 @@ class Battleship
 
   def banner
     system "clear"
-    puts " ____        _   _   _       _____ _     _       "
-    puts "|  _ \\      | | | | | |     / ____| |   (_)      "
-    puts "| |_) | __ _| |_| |_| | ___| (___ | |__  _ _ __  "
-    puts "|  _ < / _` | __| __| |/ _ \\\\___ \\| '_ \\| | '_ \\ "
-    puts "| |_) | (_| | |_| |_| |  __/____) | | | | | |_) |"
-    puts "|____/ \\__,_|\\__|\\__|_|\\___|_____/|_| |_|_| .__/ "
-    puts "                                          | |    "
-    puts "                                          |_|  "
+    puts "      ____        _   _   _       _____ _     _       "
+    puts "     |  _ \\      | | | | | |     / ____| |   (_)      "
+    puts "     | |_) | __ _| |_| |_| | ___| (___ | |__  _ _ __  "
+    puts "     |  _ < / _` | __| __| |/ _ \\\\___ \\| '_ \\| | '_ \\ "
+    puts "     | |_) | (_| | |_| |_| |  __/____) | | | | | |_) |"
+    puts "     |____/ \\__,_|\\__|\\__|_|\\___|_____/|_| |_|_| .__/ "
+    puts "                                               | |    "
+    puts "                                               |_|  "
   end
 
   def game_start
     banner
+    puts "\n\nWould you like to (p)lay, read the (i)nstructions, or (q)uit?"
     intro_menu
   end
 
   def intro_menu
-    puts "Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
     input = get_response
     case input
       when "p" || "P"
-        binding.pry
-        intro_menu
+        start_prompt
       when "i" || "I"
         instructions
-        intro_menu
       when "q" || "Q"
         exit!
       else
-        puts "Clearly that wasn't an option\n\n"
+        puts "Clearly that wasn't an option"
         intro_menu
     end
   end
 
   def start_prompt
-    puts "I have laid out my ships on the grid."
-    puts "You now need to layout your two ships."
-    puts "The first is two units long and the"
-    puts "second is three units long."
-    puts "The grid has A1 at the top left and D4 at the bottom right."
-    puts ""
+    banner
+    puts "\n\nI have laid out my ships on the grid. You now need to layout"
+    puts "your two ships. The first is two units long and the second is"
+    puts "three units long.\n\n"
+    puts "The grid has A1 at the top left and D4 at the bottom right.\n\n"
+    puts @board.print_board
     puts "Enter the squares for the two-unit ship: "
+    input = get_response
+    if input == ""
+      game_start
+    end
   end
 
   def get_response
@@ -60,9 +62,8 @@ class Battleship
   end
 
   def instructions
-    system "clear"
-    puts "Would you like to (p)lay, read the (i)nstructions, or (q)uit?"
-    puts "Welcome to the game of BattleShip! The purpose of the game is to sink"
+    banner
+    puts "\n\nWelcome to the game of BattleShip! The purpose of the game is to sink"
     puts "your opponents battleships while yours stay afloat. You will start the"
     puts "game by placing your ships on the board vertically or horizontally and"
     puts "your opponent will do the same. Once the game starts you have to find"
