80fa812364700b1686852f7ee8d85eb5f9c40c2f
jayphodges <jayphodges@gmail.com>
2017-07-17 13:53:39 -0600

    Add ai renamed to computer, inproved computer functionality

--- lib/computer.rb
+++ lib/computer.rb
@@ -5,16 +5,27 @@ require './lib/ship'
 class Computer
 
   def initialize
-    @availale = ["a1", "a2", "a3", "a4",
-                 "b1", "b2", "b3", "b4",
-                 "c1", "c2", "c3", "c4",
-                 "d1", "d2", "d3", "d4"]
+    # @availale = ["a1", "a2", "a3", "a4",
+    #              "b1", "b2", "b3", "b4",
+    #              "c1", "c2", "c3", "c4",
+    #              "d1", "d2", "d3", "d4"]
     @ship = Ship.new
   end
 
+  def available
+    ["a1", "a2", "a3", "a4",
+     "b1", "b2", "b3", "b4",
+     "c1", "c2", "c3", "c4",
+     "d1", "d2", "d3", "d4"]
+   end
+
    def create_two_unit_ship
-     coord = @available.sample
+     coord = coordinate_generator
      go_adjacent(coord)
+   end
+
+   def coordinate_generator
+     ('a'..'d').to_a.sample + ('1'..'4').to_a.sample
    end
 
    def go_adjacent(coord)
@@ -27,51 +38,52 @@ class Computer
      when 2
        go_left(coord)
      when 3
-       go_right(coordinates)
+       go_right(coord)
      end
    end
 
      def go_up(input)
-       second_coordinate = @ship.letter_up(coord[0])
+       second_coordinate = @ship.character_up(input[0])
        if @ship.valid_letter(second_coordinate)
          binding.pry
        else
          binding.pry
-         go_adjacent(coord)
+         go_adjacent(input)
        end
      end
 
      def go_down(input)
-       second_coordinate = @ship.letter_down(coord[0])
+       second_coordinate = @ship.character_down(input[0])
        if @ship.valid_letter(second_coordinate)
         binding.pry
          # valid combination, push coordinates
        else
         binding.pry
-         go_adjacent(coord)
+         go_adjacent(input)
        end
      end
 
      def go_left(input)
-       second_coordinate = @ship.letter_down(coord[1])
-       if @ship.valid_letter(second_coordinate)
+       second_coordinate = @ship.character_down(input[1])
+       if @ship.valid_number(second_coordinate)
          binding.pry
        else
          binding.pry
-         go_adjacent(coord)
+         go_adjacent(input)
        end
      end
 
      def go_right(input)
-       second_coordinate = @ship.letter_up(coord[1])
-       if @ship.valid_letter(second_coordinate)
+       second_coordinate = @ship.character_up(input[1])
+       if @ship.valid_number(second_coordinate)
         binding.pry
          # valid combination, push coordinates
        else
         binding.pry
-         go_adjacent(coord)
+         go_adjacent(input)
        end
      end
 end
-cpu = computer.new
+cpu = Computer.new
+cpu.create_two_unit_ship
 binding.pry
