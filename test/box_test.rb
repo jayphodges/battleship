db0b5f1b84b80ae54186c95ede40bb8d4eca38d7
jayphodges <jayphodges@gmail.com>
2017-07-16 23:39:14 -0600

    Temporarily removed simplecov

--- test/box_test.rb
+++ test/box_test.rb
@@ -1,5 +1,5 @@
-require 'simplecov'
-SimpleCov.start
+# require 'simplecov'
+# SimpleCov.start
 require './lib/box'
 require 'pry'
 require 'minitest/autorun'
--- test/ship_test.rb
+++ test/ship_test.rb
@@ -1,5 +1,5 @@
-require 'simplecov'
-SimpleCov.start
+# require 'simplecov'
+# SimpleCov.start
 require './lib/ship'
 require 'pry'
 require 'minitest/autorun'
