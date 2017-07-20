9bb8ae3a6d29f15de234636f63e31e79968fd700
jayphodges <jayphodges@gmail.com>
2017-07-20 08:50:59 -0600

    Add additional tests and comment removal

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -1,5 +1,5 @@
-# require 'simplecov'
-# SimpleCov.start
+require 'simplecov'
+SimpleCov.start
 require './lib/ship'
 require 'pry'
 require 'minitest/autorun'
@@ -37,8 +37,8 @@ class ShipTest < Minitest::Test
   end
 
   def test_for_character_ups
-    assert_equal "c", @ship.character_up("a")
-    assert_equal "Y", @ship.character_up("W")
+    assert_equal "c", @ship.character_ups("a")
+    assert_equal "Y", @ship.character_ups("W")
   end
 
   def test_for_adjacent_units
@@ -48,28 +48,36 @@ class ShipTest < Minitest::Test
     refute @ship.adjacent_units("a","c")
   end
 
-  def test_vertical_and_horizontal_check
+  def test_two_unit_adjacency
+    assert @ship.two_unit_adjacency(["a1", "a2"])
+    assert @ship.two_unit_adjacency(["b9", "f9"])
+    refute @ship.two_unit_adjacency(["bb", "dd"])
+  end
 
+  def test_three_unit_adjacency_letters
+    skip
+    assert @ship.three_unit_adjacency_letters(["a1", "a2", "a3"])
+    assert @ship.three_unit_adjacency_letters(["h9", "hd", "h5"])
+    refute @ship.three_unit_adjacency_letters(["d0", "a1", "d9"])
+  end
+
+  def test_three_unit_adjacency_numbers
+    skip
+    assert @ship.three_unit_adjacency_numbers(["a1", "b1", "c1"])
+    assert @ship.three_unit_adjacency_numbers(["09", "r9", "a9"])
+    refute @ship.three_unit_adjacency_numbers(["d9", "a1", "d9"])
   end
 
   def test_converting_to_pairs
+    skip
     assert_equal ["11","11","11"],
      @ship.convert_to_pairs("111111")
     assert_equal ["Ab","Cd"],
      @ship.convert_to_pairs("AbCdE")
   end
 
-  def test_input_formatter
-    skip
-    assert_equal ["A1","B2"],
-     @ship.format_input("a1b2")
-    assert_equal ["AA", "ZZ"],
-     @ship.format_input("aaZz")
-    assert_equal ["AS", "DF", "12", "34"],
-     @ship.format_input("asdf1234")
-  end
-
   def test_two_unit_adjacency
+    skip
     assert @ship.two_unit_adjacency(["a1", "b1"])
   end
 
